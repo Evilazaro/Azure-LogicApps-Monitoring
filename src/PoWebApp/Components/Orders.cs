60f5f1e0e63b6af1219c843f39ea4f5b8269c803
Evilazaro Alves <evalvesd@microsoft.com>
2025-12-08 09:25:14 -0600

    Fix resourceName syntax in azure.yaml and add Orders component for queue messaging

---
+++ src/PoWebApp/Components/Orders.cs
@@ -0,0 +1,12 @@
+﻿namespace PoWebApp.Components
+{
+    public class Orders
+    {
+        public async Task AddOrderMessageToQueueAsync(string connectionString, string queueName, string message)
+        {
+            var queueClient = new QueueClient(connectionString, queueName);
+            await queueClient.CreateIfNotExistsAsync();
+            await queueClient.SendMessageAsync(message);
+        }
+    }
+}
