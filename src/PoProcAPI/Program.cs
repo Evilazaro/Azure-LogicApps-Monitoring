dc13010dd49e6b4c57443a6700c0da0214910440
Evilazaro Alves <evalvesd@microsoft.com>
2025-12-09 12:16:08 -0600

    Add Swagger/OpenAPI support with Swashbuckle packages
    Added Swashbuckle.AspNetCore packages to enable Swagger/OpenAPI documentation and UI for the API. Updated Program.cs to register Swagger services and middleware, making the Swagger UI and OpenAPI endpoints available in all environments, not just development.

--- src/PoProcAPI/Program.cs
+++ src/PoProcAPI/Program.cs
@@ -1,3 +1,4 @@
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -5,14 +6,14 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.MapOpenApi();
-}
+app.MapOpenApi();
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 
