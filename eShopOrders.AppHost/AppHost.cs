2d908e75a572df0693125543d54a27d48fe08917
Evilazaro Alves <evalvesd@microsoft.com>
2025-12-11 13:37:12 -0600

    Add eShop.Orders.API project and implement WeatherForecast API with OpenAPI support

--- eShopOrders.AppHost/AppHost.cs
+++ eShopOrders.AppHost/AppHost.cs
@@ -2,4 +2,6 @@ var builder = DistributedApplication.CreateBuilder(args);
 
 builder.AddProject<Projects.eShop_Orders_App>("eshop-orders-app");
 
+builder.AddProject<Projects.eShop_Orders_API>("eshop-orders-api");
+
 builder.Build().Run();
