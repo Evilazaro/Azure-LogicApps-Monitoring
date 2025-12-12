12d2f25d0101be8006384653f27c34b3677d325a
Evilazaro Alves <evalvesd@microsoft.com>
2025-12-11 18:53:18 -0600

    refactor: Integrate Application Insights connection string into orders API and webapp configurations

--- eShopOrders.AppHost/AppHost.cs
+++ eShopOrders.AppHost/AppHost.cs
@@ -1,11 +1,20 @@
+using Aspire.Hosting;
+using Aspire.Hosting.Azure;
 using Projects;
+
 
 var builder = DistributedApplication.CreateBuilder(args);
 
+var appInsights = builder.AddAzureApplicationInsights("app-insights");
+
+var outputs = appInsights.Resource.ConnectionString;
+
 var ordersApi = builder.AddProject<Projects.eShop_Orders_API>("orders-api")
+    .WithEnvironment("APPLICATIONINSIGHTS_CONNECTION_STRING", outputs)
     .WithEnvironment("ASPNETCORE_ENVIRONMENT", "Development");
 
 var ordersWebApp = builder.AddProject<eShop_Orders_App>("orders-webapp")
+    .WithEnvironment("APPLICATIONINSIGHTS_CONNECTION_STRING", outputs)
     .WithReference(ordersApi).WaitFor(ordersApi);
 
 builder.Build().Run();
