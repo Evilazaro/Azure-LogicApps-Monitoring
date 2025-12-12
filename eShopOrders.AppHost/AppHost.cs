7230dc80d8941e9812129e58af33a0da5377f598
Evilazaro Alves <evalvesd@microsoft.com>
2025-12-11 18:30:31 -0600

    refactor: Add ASPNETCORE_ENVIRONMENT configuration to orders API project

--- eShopOrders.AppHost/AppHost.cs
+++ eShopOrders.AppHost/AppHost.cs
@@ -2,7 +2,8 @@ using Projects;
 
 var builder = DistributedApplication.CreateBuilder(args);
 
-var ordersApi = builder.AddProject<Projects.eShop_Orders_API>("orders-api");
+var ordersApi = builder.AddProject<Projects.eShop_Orders_API>("orders-api")
+    .WithEnvironment("ASPNETCORE_ENVIRONMENT", "Development");
 
 var ordersWebApp = builder.AddProject<eShop_Orders_App>("orders-webapp")
     .WithReference(ordersApi).WaitFor(ordersApi);
