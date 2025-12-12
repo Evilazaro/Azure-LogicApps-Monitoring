111dcfef2a77580827bd2d8b9a8cdaf37cbc6780
Evilazaro Alves <evalvesd@microsoft.com>
2025-12-11 19:28:17 -0600

    refactor: Enhance Application Insights integration and update infrastructure configurations for orders API and webapp

--- eShopOrders.AppHost/AppHost.cs
+++ eShopOrders.AppHost/AppHost.cs
@@ -1,20 +1,20 @@
 using Aspire.Hosting;
 using Aspire.Hosting.Azure;
+using Microsoft.VisualBasic;
 using Projects;
-
 
 var builder = DistributedApplication.CreateBuilder(args);
 
-var appInsights = builder.AddAzureApplicationInsights("app-insights");
-
-var outputs = appInsights.Resource.ConnectionString;
-
 var ordersApi = builder.AddProject<Projects.eShop_Orders_API>("orders-api")
-    .WithEnvironment("APPLICATIONINSIGHTS_CONNECTION_STRING", outputs)
-    .WithEnvironment("ASPNETCORE_ENVIRONMENT", "Development");
+    .WithEnvironment("APPLICATIONINSIGHTS_CONNECTION_STRING", eShopOrders.AppHost.Constants.APPLICATIONINSIGHTS_CONNECTION_STRING)
+    .WithEnvironment("ASPNETCORE_ENVIRONMENT", "Development")
+    .AsHttp2Service()
+    .WithExternalHttpEndpoints();
 
 var ordersWebApp = builder.AddProject<eShop_Orders_App>("orders-webapp")
-    .WithEnvironment("APPLICATIONINSIGHTS_CONNECTION_STRING", outputs)
-    .WithReference(ordersApi).WaitFor(ordersApi);
+    .WithEnvironment("APPLICATIONINSIGHTS_CONNECTION_STRING", eShopOrders.AppHost.Constants.APPLICATIONINSIGHTS_CONNECTION_STRING)
+    .WithReference(ordersApi).WaitFor(ordersApi)
+    .AsHttp2Service()
+    .WithExternalHttpEndpoints();
 
 builder.Build().Run();
---
+++ eShopOrders.AppHost/Constants.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eShopOrders.AppHost
+{
+    public static class Constants
+    {
+        public const string APPLICATIONINSIGHTS_CONNECTION_STRING = "{{ .Env.AZURE_APPLICATION_INSIGHTS_CONNECTION_STRING }}";
+    }
+}
