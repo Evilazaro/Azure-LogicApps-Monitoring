1ee976d53d29a7390653498eb14666322a4a2777
Evilazaro Alves <evalvesd@microsoft.com>
2025-12-18 17:48:39 -0600

    Refactor application configuration for eshop-web-app and add eshop-orders-api template

--- app.AppHost/AppHost.cs
+++ app.AppHost/AppHost.cs
@@ -1,6 +1,9 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
 var ordersAPI = builder.AddProject<Projects.eShop_Orders_API>("eshop-orders-api");
-var webApp = builder.AddProject<Projects.eShop_Web_App>("eshop-web-app");
+
+var webApp = builder.AddProject<Projects.eShop_Web_App>("eshop-web-app")
+                    .WithReference(ordersAPI)
+                    .WaitFor(ordersAPI);
 
 builder.Build().Run();
