3e8e96852f524d3ed6a66911d6c5caf77699d58e
Evilazaro Alves <evalvesd@microsoft.com>
2025-12-18 18:03:12 -0600

    Refactor service registration for orders API and web app to use HTTP2 and external endpoints

--- app.AppHost/AppHost.cs
+++ app.AppHost/AppHost.cs
@@ -1,9 +1,12 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
-var ordersAPI = builder.AddProject<Projects.eShop_Orders_API>("eshop-orders-api");
+var ordersAPI = builder.AddProject<Projects.eShop_Orders_API>("eshop-orders-api")
+                       .WithExternalHttpEndpoints()
+                       .AsHttp2Service();
 
 var webApp = builder.AddProject<Projects.eShop_Web_App>("eshop-web-app")
+                    .WithExternalHttpEndpoints()
                     .WithReference(ordersAPI)
-                    .WaitFor(ordersAPI);
+                    .AsHttp2Service();
 
 builder.Build().Run();
