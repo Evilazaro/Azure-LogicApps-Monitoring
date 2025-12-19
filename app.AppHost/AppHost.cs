7fe4e747019dc77c11c18aa7f2f5a079a8141ad2
Evilazaro Alves <evalvesd@microsoft.com>
2025-12-18 19:34:49 -0600

    refactor: Remove Docker Compose environment and related package reference

--- app.AppHost/AppHost.cs
+++ app.AppHost/AppHost.cs
@@ -3,8 +3,6 @@ var builder = DistributedApplication.CreateBuilder(args);
 var ordersAPI = builder.AddProject<Projects.eShop_Orders_API>("orders-api")
                        .WithHttpHealthCheck("/health");
 
-builder.AddDockerComposeEnvironment("prod");
-
 var webApp = builder.AddProject<Projects.eShop_Web_App>("web-app")
                     .WithExternalHttpEndpoints()
                     .WithHttpHealthCheck("/health")
